4d094711ed8b19603ab88f558c70b131098686d9
a.bondarenko <kballarock666@gmail.com>
2020-04-02 16:28:31 +0300

    - refactor restaurant test data

--- src/test/java/by/bestlunch/RestaurantTestData.java
+++ src/test/java/by/bestlunch/RestaurantTestData.java
@@ -4,7 +4,6 @@ import by.bestlunch.persistence.model.Restaurant;
 
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.List;
 
 import static java.time.LocalDateTime.of;
 
@@ -12,8 +11,6 @@ public class RestaurantTestData {
 
     public static final Restaurant RESTAURANT_BURGER_KING = new Restaurant(100000, "Burger King", "Быстрое питание", "г. Минск. ул. Кирова, 10", of(2019, Month.DECEMBER, 16, 1, 0));
     public static final Restaurant RESTAURANT_RENAISSANCE = new Restaurant(100001, "Renaissance", "Итальянская кухня", "г. Минск, ул. Сурганова, 5", of(2019, Month.DECEMBER, 16, 2, 0));
-
-    public static final List<Restaurant> RESTAURANTS = List.of(RESTAURANT_BURGER_KING, RESTAURANT_RENAISSANCE);
 
     public static Restaurant getNew() {
         return new Restaurant(null, "NewRestaurant", "NewKitchen", "NewAddress", LocalDateTime.now());
