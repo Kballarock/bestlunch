af242e6a54cf240b469e0f637560e11f10b13a90
a.bondarenko <kballarock666@gmail.com>
2020-04-27 01:25:07 +0300

    - try fix travis

--- src/test/java/by/bestlunch/service/AbstractServiceTest.java
+++ src/test/java/by/bestlunch/service/AbstractServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.transaction.annotation.Transactional;
 
 import static by.bestlunch.validation.ValidationUtil.getRootCause;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringJUnitWebConfig(AbstractServiceTest.Config.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ExtendWith(TimingExtension.class)
+@Transactional
 abstract class AbstractServiceTest {
 
     @Configuration
